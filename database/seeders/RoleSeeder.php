88f5cb30d0f3147ca2e358cd5c8f6c9db81bb6be
idril150 <ebibari1@gmail.com>
2023-02-07 09:52:30 -0600

    inicio

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class roleseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $Role1 = Role::create(['name' => 'Admin']);
+        $Role2 = Role::ccreate(['name' => 'JefAr']);
+
+        Permission::create(['name' => 'kk']);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    /*return view('welcome');*/
+    return "holi crayoli";
+});
+
+Route::get('encuestas', function(){
+    return "holi";
+});
+
+Route::get('encuestas/create', function () {
+    return "en esta pagina se puede crear un formulario";
+});
+
+Route::get('encuestas/{encuesta}', function($encuesta){
+    return "bienvenido a la encuesta: $encuesta ";
+
+});
