6075a3d969dc35c19bc8da93b5bc872913299c7f
idril150 <ebibari1@gmail.com>
2023-02-07 10:51:54 -0600

    a

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,6 @@ class HomeController extends Controller
 {
     public function __invoke()
     {
-        return "ctm";
+        return "hola todos";
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-use app\Http\Controllers\HomeController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', HomeController::class);
 
